e74ade30024f9f06da4f37df4dd3a3c404519410
Jarigo <johnarigo96706@gmail.com>
2026-02-03 00:29:56 -1000

    Updates

--- src/data/buildOrg.ts
+++
@@ -1,9 +0,0 @@
-import { generateOrg, levelOrder } from './generateOrg'
-import { computeScores } from './computeScores'
-import type { OrgData, OrgProfile } from './types'
-
-export const buildOrg = (seed: number, profile: OrgProfile = 'balanced'): OrgData => {
-  const org = generateOrg(seed, profile)
-  computeScores(org.nodesById, levelOrder)
-  return org
-}
